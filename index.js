4bb4c03e5b55b61d6080f4c7ad37df666966f396
Florian Wagner <florian.wagner@aut.uni-saarland.de>
2018-02-17 15:52:57 +0100

    Added copyright and and license header to every source file.

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+// dwd_forecast_service
+//
+// Copyright 2018 The dwd_forecast_service Developers. See the LICENSE file at
+// the top-level directory of this distribution and at
+// https://github.com/UdSAES/dwd_forecast_service/LICENSE
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+// dwd_forecast_service may be freely used an distributed under the MIT license
+
 'use strict'
 
 const express = require('express')
