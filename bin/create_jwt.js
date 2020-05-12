29bf2662607612363726f073a369ead986b9fd50
Moritz Stüber <moritz.stueber@aut.uni-saarland.de>
2020-05-12 13:56:56 +0200

    Update script to generate token -- does _not_ work!
    Verification fails -> investigate or remove.

--- bin/create_jwt.js
+++ bin/create_jwt.js
@@ -3,12 +3,36 @@
 const jwt = require('jsonwebtoken')
 const fs = require('fs-extra')
 
-var cert = fs.readFileSync('./sample_data/sample_private_key.pem')
-jwt.sign({ sub: 'test1@msaas.me' }, cert, { algorithm: 'RS256' }, function (error, data) {
-  if (error != null) {
-    console.log(error)
-    return
-  }
+// Get key pair -- MUST be in PEM format
+// The private key usally already is in PEM format, use
+// `ssh-keygen -e -m pem -f id_rsa.pub` to convert the public key
+// const privateKey = './sample_data/sample_private_key.pem'
+const privateKey = './local/jwt_private_key_nibbler.pem'
+const publicKey = './local/jwt_public_key_nibbler.pem'
 
-  console.log(data)
-})
+var privateCert = fs.readFileSync(privateKey, { encoding: 'utf8' })
+var publicCert = fs.readFileSync(publicKey, { encoding: 'utf8' })
+
+// Create token and sign using the private key
+const token = jwt.sign(
+  {
+    iss: 'UdS AES',
+    sub: 'UdS AES'
+  },
+  privateCert,
+  { algorithm: 'RS256' }
+)
+console.log(token)
+
+// // Decode token without verification of signature
+// const decoded = jwt.decode(token)
+// console.log(decoded)
+
+// Decode and verify token
+try {
+  const verified = jwt.verify(token, publicCert)
+  console.log('\nverified!')
+  console.log(verified)
+} catch (error) {
+  console.log(error)
+}
