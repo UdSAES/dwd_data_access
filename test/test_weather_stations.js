7dfc8f383f5d306ae97a17a9381ea97eba65664e
Moritz Stüber <moritz.stueber@aut.uni-saarland.de>
2020-05-14 18:49:10 +0200

    Clean up implementation of existing tests

--- test/test_weather_stations.js
+++ test/test_weather_stations.js
@@ -12,22 +12,23 @@ const sc = require('../lib/weather_stations')
 
 const DWD_STATIONS_MOSMIX_FROM_PDF = './config/dwd2017_stations_mosmix_from_pdf.txt'
 
-describe('Test correct parsing of station catalogue(s)', () => {
-  describe('Do not throw upon reading the station catalogue from the .txt file', () => {
-    it('should return a list of stations', async () => {
+describe('Test correct parsing of station catalogue(s)', function () {
+  describe('Do not throw upon reading the station catalogue from the old .txt file', function () {
+    it('should return a list of stations', async function () {
       const stationData = await sc.readStationsMosmixFromTxt(DWD_STATIONS_MOSMIX_FROM_PDF)
     }).timeout(0)
   })
 })
 
-describe('Test correct identification of weather stations in the vicinity of given coordinates', () => {
-  var stations = null
-  before(async () => {
+describe('Test correct identification of weather stations in the vicinity of given coordinates', function () {
+  let stations = null
+
+  before(async function () {
     stations = await sc.readStationsMosmixFromTxt(DWD_STATIONS_MOSMIX_FROM_PDF)
   })
 
-  describe('Find _the_ single closest station', () => {
-    it('should return a list of stations', async () => {
+  describe('Find _the_ single closest station', function () {
+    it('should return a list of stations', async function () {
       const closestStation = await su.findClosestStation({ longitude: 11.11, latitude: 60.19 }, stations)
       console.log(closestStation)
     }).timeout(0)
