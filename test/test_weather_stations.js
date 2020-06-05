81907ea31ad658b68b6e4930b3896a3f0d7c7ade
Moritz Stüber <moritz.stueber@aut.uni-saarland.de>
2020-06-05 19:00:43 +0200

    Remove code, data for reading MOSMIX-stations from .txt-file from .pdf
    ..because that file was an unreliable/outdated data source at best and 
    alternatives are now available.

--- test/test_weather_stations.js
+++ test/test_weather_stations.js
@@ -13,17 +13,9 @@ const addContext = require('mochawesome/addContext')
 const su = require('../lib/station_utils')
 const sc = require('../lib/weather_stations')
 
-const DWD_STATIONS_MOSMIX_FROM_PDF = './config/dwd2017_stations_mosmix_from_pdf.txt'
-
 describe('Test correct parsing of station catalogue(s)', function () {
   const dwdStationsBeobHaAsCSV = './test/data/dwd2017_stations_beob_ha.csv'
   const dwdStationsBeobNaAsCSV = './test/data/dwd2018_stations_beob_na.csv'
-
-  describe('Do not throw upon reading the station catalogue from the old .txt file', function () {
-    it('should return a list of stations', async function () {
-      const stationData = await sc.readStationsMosmixFromTxt(DWD_STATIONS_MOSMIX_FROM_PDF)
-    }).timeout(0)
-  })
 
   describe('Parse the list of major weather stations used by DWD', async function () {
     const dwdStationsBeobHaAsJSON = './test/data/dwd2017_stations_beob_ha.json'
