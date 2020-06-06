99b122003d7ce48bda754fbe0745a1994193ed1d
Moritz Stüber <moritz.stueber@aut.uni-saarland.de>
2020-06-06 10:28:41 +0200

    Clean up script to generate JWTs

--- auth/create_jwt.js
+++ auth/create_jwt.js
@@ -1,14 +1,17 @@
+// Script to generate a JSON web token to be used for authenticating with the API
+
 'use strict'
 
-const jwt = require('jsonwebtoken')
 const fs = require('fs-extra')
+const jwt = require('jsonwebtoken')
 
 // Get key pair -- MUST be in PEM format
-// The private key usally already is in PEM format, use
+// The private key usually already is in PEM format, use
 // `ssh-keygen -e -m pem -f id_rsa.pub` to convert the public key
-// const privateKey = './sample_data/sample_private_key.pem'
-const privateKey = './local/jwt_private_key_nibbler.pem'
-const publicKey = './local/jwt_public_key_nibbler.pem'
+const privateKey = './auth/sample_private_key.pem'
+const publicKey = './auth/sample_public_key.pem'
+// const privateKey = './local/jwt_private_key_nibbler.pem' // for deployment
+// const publicKey = './local/jwt_public_key_nibbler.pem' // for deployment
 
 var privateCert = fs.readFileSync(privateKey, { encoding: 'utf8' })
 var publicCert = fs.readFileSync(publicKey, { encoding: 'utf8' })
@@ -17,7 +20,7 @@ var publicCert = fs.readFileSync(publicKey, { encoding: 'utf8' })
 const token = jwt.sign(
   {
     iss: 'UdS AES',
-    sub: 'UdS AES'
+    sub: 'wetternachhersage.de'
   },
   privateCert,
   { algorithm: 'RS256' }
