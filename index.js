346b60521c189cf6c4774f2e1782411262c88efc
Moritz Stüber <moritz.stueber@aut.uni-saarland.de>
2020-07-03 19:05:53 +0200

    Expose new OAS as /oas instead of old one
    Enables sending requests via Swagger-UI

--- index.js
+++ index.js
@@ -208,7 +208,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded())
 app.use(express.static('./docs'))
-app.use('/oas', express.static('./docs/openapi_oas2.json'))
+app.use('/oas', express.static('./docs/openapi_oas3.json'))
 
 // Expose UI iff UI_URL_PATH is not empty
 if (UI_URL_PATH !== '') {
